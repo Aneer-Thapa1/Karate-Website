a1b0203d6abb4b413eed887686a1765a641b78f1
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2025-01-20 21:35:44 +0545

    store and features added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Landing from "./pages/Landing.jsx";
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 function App() {
   return (
+      <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
@@ -14,6 +16,7 @@ function App() {
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
+      </Provider>
   );
 }
 
