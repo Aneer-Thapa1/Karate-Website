6f2da1363b7ed804bf9968318511042167e5435c
Anir Jung Thapa <134839446+Aneer-Thapa1@users.noreply.github.com>
2025-01-21 15:55:13 +0545

    admin side started

---
+++ client/src/pages/Bookings.jsx
@@ -0,0 +1,8 @@
+
+
+const Bookings = () => {
+    return (
+        <div>Bookings for admin is hown here!</div>
+    )
+}
+export default Bookings
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -2,12 +2,12 @@ import Navbar from "../components/Navbar.jsx";
 
 
 const Landing = () => {
-    return (
-        <div className="relative bg-cover bg-center bg-no-repeat min-h-screen w-screen dark:bg-background-dark">
-                <Navbar/>
+    return (<div className="relative bg-cover bg-center bg-no-repeat min-h-screen w-screen dark:bg-background-dark">
+            <Navbar/>
+            <div className=''>
 
-        </div>
-    )
+            </div>
+        </div>)
 }
 
 export default Landing
